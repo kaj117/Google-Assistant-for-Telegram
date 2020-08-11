dfe40a911aaef21b43856e62544e4be44a8fc411
DarK Angel <60337323+DarkAngelTG@users.noreply.github.com>
2020-08-11 19:58:15 +0530

    Update telegramassistant.py

--- telegramassistant.py
+++ telegramassistant.py
@@ -41,15 +41,15 @@ except (SystemError, ImportError):
 
 ASSISTANT_API_ENDPOINT = 'embeddedassistant.googleapis.com'
 DEFAULT_GRPC_DEADLINE = 60 * 3 + 5
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = os.environ.get('1273164107:AAH4tsYkztBceq1VMeQ2LfrBqUz47nnK6yg')
 ALLOWED_CHAT_IDS = list(
-        map(int, os.environ.get('ALLOWED_CHAT_IDS').split(','))
+        map(int, os.environ.get('-1001380038374').split(','))
         )
 AUTHORIZED_USER_IDS = list(
-        map(int, os.environ.get('AUTHORIZED_USER_IDS').split(','))
+        map(int, os.environ.get('1155642873').split(','))
         )
-DEVICE_MODEL_ID = os.environ.get('DEVICE_MODEL_ID')
-DEVICE_ID = os.environ.get('DEVICE_ID')
+DEVICE_MODEL_ID = os.environ.get('ASUS_X00TD')
+DEVICE_ID = os.environ.get('J55AAB760J692F26')
 
 
 class SampleTextAssistant(object):
